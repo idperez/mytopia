8b58322bac7dca4edbbc22ddccd587e64052949e
isidro <idperez@asu.edu>
2017-03-10 21:24:15 -0700

    header and tabs

---
+++ app/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import { Tabs, Tab } from 'react-native-elements';
+
+import Header from './../Headers/Header';
+
+class Home extends Component {
+    render () {
+        return (<View>
+            <Header/>
+        </View>);
+    }
+
+}
+
+export default Home;
