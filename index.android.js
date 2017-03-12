b79f6da7a081f7811dc80499ccd714b210648dcf
jerss2k <dcarrol3@asu.edu>
2017-03-11 23:35:54 -0700

    city info examples

--- index.android.js
+++ index.android.js
@@ -26,7 +26,7 @@ class Blink extends Component {
         super(props);
         this.state = {showText: true};
 
-        todoSearchResults.getTodoList('austin', 'tx').then((res)=>{
+        CityPageData.getCityInfo('austin', 'tx').then((res)=>{
             test = JSON.stringify(res);
             this.setState({showText: true})
         });
