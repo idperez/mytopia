397d6d1c0fa73ea3017433d60915b16abb4c811a
isidro <idperez@asu.edu>
2017-03-12 02:43:03 -0700

    preferences page

--- app/Home/Preferences.js
+++ app/Home/Preferences.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, TextInput } from 'react-native';
 
-import { Icon } from 'react-native-elements'
+import { Icon, Card, Button } from 'react-native-elements'
+
 
 let results = "";
 
@@ -27,6 +28,72 @@ class Preferences extends Component {
                     <Icon
                         name="settings"
                     />
+                    <Text style={styles.heroText}> Your Preferences</Text>
+                </View>
+                <View >
+                    <Card
+                        title='Edit Job Preferences'>
+                        <Text style={styles.cardText}>Job Title</Text>
+                        <TextInput
+                            style = {styles.cardText}
+                            placeholder = 'Software Engineer'
+                        />
+                        <Button
+                            icon={{name: 'work'}}
+                            backgroundColor='#8dc63f'
+                            buttonStyle={{marginTop: 5, borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            title='Save Job Preferences'
+                            />
+                    </Card>
+                    <Card
+                        title='Edit Housing Preferences'>
+                        <Text style={styles.cardText}>Min Price</Text>
+                        <TextInput
+                            style = {styles.cardText}
+                            placeholder = '$1000'
+                        />
+                        <Text style={styles.cardText}>Max Price</Text>
+                        <TextInput
+                            style = {styles.cardText}
+                            placeholder = '$2300'
+                        />
+                        <Text style={styles.cardText}>Bedrooms</Text>
+                        <TextInput
+                            style = {styles.cardText}
+                            placeholder = '1'
+                        />
+                        <Text style={styles.cardText}>Bathrooms</Text>
+                        <TextInput
+                            style = {styles.cardText}
+                            placeholder = '1'
+                        />
+                        <Button
+                            icon={{name: 'home'}}
+                            backgroundColor='#8dc63f'
+                            buttonStyle={{marginTop: 5, borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            title='Save Housing Preferences'
+                        />
+                    </Card>
+                    <Card
+                        title='Edit Things To Do Preferences'>
+                        <Text style={styles.cardText}>Food</Text>
+                        <TextInput
+                            style = {styles.cardText}
+                            placeholder = 'Italian'
+                        />
+                        <Text style={styles.cardText}>Fun</Text>
+                        <TextInput
+                            style = {styles.cardText}
+                            placeholder = 'Hiking, Movies'
+                        />
+                        <Button
+                            icon={{name: 'favorite'}}
+                            backgroundColor='#8dc63f'
+                            buttonStyle={{marginTop: 5, borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+                            title='Save Job Preferences'
+                        />
+                    </Card>
+
                 </View>
             </ScrollView>);
     }
@@ -49,8 +116,11 @@ const styles = StyleSheet.create({
         marginTop: 60
     },
     cardText: {
-        marginBottom: 10,
+        marginBottom: 15,
         textAlign: 'center',
+    },
+    heroText: {
+        marginTop: 5
     },
     logInButton: {
         marginBottom: 5
